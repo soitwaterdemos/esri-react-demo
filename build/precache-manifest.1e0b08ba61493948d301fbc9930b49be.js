67e789a9fb75b62aaaa4c0b1537fd2d0505c2bf8
csy <kisscsy@foxmail.com>
2019-12-24 09:30:10 +0800

    add 打包完的内容

---
+++ build/precache-manifest.1e0b08ba61493948d301fbc9930b49be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bd984d95b5105cf694243f77a874d67",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2a9c49f71dc205bf5048",
+    "url": "./static/js/2.3ba7ad3d.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "./static/js/2.3ba7ad3d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "83eff3c7603a1904fc17",
+    "url": "./static/js/main.956ad948.chunk.js"
+  },
+  {
+    "revision": "a3873b39789d1999c0fe",
+    "url": "./static/js/runtime-main.4baa3d0e.js"
+  }
+]);
