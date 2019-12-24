aa2e3bcac422201ecf43338b48319d90833d1693
csy <kisscsy@foxmail.com>
2019-12-24 09:13:16 +0800

    上线

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom';
 import Text from '../pages/text/index.js'
 import Map from '../pages/map/index.js'
 
 const BasicRoute = () => (
-  <BrowserRouter>
+  <HashRouter>
     <Route exact path="/text" component={Text} />
     <Route exact path="/" component={Map} />
     <Route exact path="/map" component={Map} />
-  </BrowserRouter>
+  </HashRouter>
 )
 
 export default BasicRoute
